ed3456bcfb959074f5b47fd93079419b0e49e78c
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-24 19:41:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
@@ -12,6 +12,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
       self.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
